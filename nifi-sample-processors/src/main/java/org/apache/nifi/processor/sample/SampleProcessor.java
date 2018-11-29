28618a1dfe31432e912005c138c2b159a1928fe0
Mike Thomsen <mikerthomsen@gmail.com>
2018-11-29 07:51:06 -0500

    Build works.

---
+++ nifi-sample-processors/src/main/java/org/apache/nifi/processor/sample/SampleProcessor.java
@@ -0,0 +1,13 @@
+package org.apache.nifi.processor.sample;
+
+import org.apache.nifi.processor.AbstractProcessor;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.exception.ProcessException;
+
+public class SampleProcessor extends AbstractProcessor {
+    @Override
+    public void onTrigger(ProcessContext processContext, ProcessSession processSession) throws ProcessException {
+
+    }
+}
---
+++ nifi-sample-processors/src/test/groovy/org/apache/nifi/processor/sample/SampleProcessorTest.groovy
@@ -0,0 +1,10 @@
+package org.apache.nifi.processor.sample
+
+import org.junit.Test
+
+class SampleProcessorTest {
+    @Test
+    void test() {
+
+    }
+}
