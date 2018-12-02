817be2c9ad1e1051125a5b459934153a64462f7d
Mike Thomsen <mikerthomsen@gmail.com>
2018-12-02 07:27:12 -0500

    Added service tests.

---
+++ nifi-sample-services/src/test/groovy/org/apache/nifi/sample/SampleLookupServiceTest.groovy
@@ -0,0 +1,38 @@
+/*
+ * Sample notice if you want to submit work to the ASF:
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.nifi.sample
+
+import org.apache.nifi.util.TestRunners
+import static org.junit.Assert.*
+import org.junit.Test
+
+class SampleLookupServiceTest {
+    @Test
+    void test() {
+        def runner = TestRunners.newTestRunner(MockProcessor.class)
+        def service = new SampleLookupService()
+        runner.addControllerService("lookup", service)
+        runner.setProperty(MockProcessor.LOOKUP, "lookup")
+        runner.enableControllerService(service)
+        runner.assertValid()
+
+        assertNotNull(service.lookup([email: "test@test.com", name: "Test User"]))
+    }
+}
